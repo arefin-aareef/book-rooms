2f5199ee999ce563633813931ec206c26b83888b
arefin_aareef <arefin.aareef@gmail.com>
2023-02-03 17:21:37 +0600

    responsive

--- client/src/screens/Landingscreen.js
+++ client/src/screens/Landingscreen.js
@@ -5,7 +5,7 @@ function Landingscreen() {
   return (
     <div className='row landing justify-content-center mx-0'>
         <div className="col-md-10 my-auto text-center">
-            <h2 style={{color: 'white', fontSize:'130px'}}>Book Rooms</h2>
+            <h2 style={{color: 'white', fontSize:'80px'}}>Book Rooms</h2>
             <h1 style={{color: 'white'}}>Get your desired room. Just a click away.</h1>
             <Link to='/home'>
                 <button className='btn landingbtn'>Get Started</button>
